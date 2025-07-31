be06103c0cdea289a79446d47cb575c150c18cff
Lala Diallo <diallo.lala@ugb.edu.sn>
2025-07-31 01:14:34 +0100

    composants header et footer

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,8 +1,10 @@
 import { Routes } from '@angular/router';
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
 
 
 export const routes: Routes = [
-    {
-      
-    }
+    
+      { path: '', component: HeaderComponent },
+    
 ];
